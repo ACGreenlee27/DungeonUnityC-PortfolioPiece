2915c912cd5764d4abb0e699d08101ac9efd35ed
AC <126024694+ACGreenlee27@users.noreply.github.com>
2024-08-18 21:36:40 -0500

    Update footSteps.cs

--- footSteps.cs
+++ footSteps.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Super simple demo script to play sounds from an audiosource when a key or key combination is pressed. 
+//In this case, this is the simple way I chose to handle footstep sounds for walking and sprinting in my demo project. 
 
 public class footsteps : MonoBehaviour
 {
